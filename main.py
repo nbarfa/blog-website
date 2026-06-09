04158ca0d10a5367a643ad109761175ef6240f2d
Nitin Barfa <nitinbarfa260@gmail.com>
2026-06-09 15:31:32 +0530

    fix the phone_number cloumn type

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class Contact(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(20), nullable=False)
-    phone_number = db.Column(db.String(12), nullable=False)
+    phone_number = db.Column(db.String(15), nullable=False)
     message = db.Column(db.Text, nullable=False)
     date = db.Column(db.String(12), nullable=True)
 
